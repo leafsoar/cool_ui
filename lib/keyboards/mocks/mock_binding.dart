4d063c8a693a7b8a28064661f50314fd2df5ae30
leafsoar <kltwjt@gmail.com>
2022-05-17 21:51:28 +0800

    v3.0

--- lib/keyboards/mocks/mock_binding.dart
+++ lib/keyboards/mocks/mock_binding.dart
@@ -1,26 +1,25 @@
 part of cool_ui;
 
-
-
 class MockBinding extends WidgetsFlutterBinding {
   @override
   void initInstances() {
     // TODO: implement initInstances
     super.initInstances();
-    _binaryMessenger  = MockBinaryMessenger(super.defaultBinaryMessenger);
+    _binaryMessenger = MockBinaryMessenger(super.defaultBinaryMessenger);
   }
 
-  
   static WidgetsBinding ensureInitialized() {
-    if (WidgetsBinding.instance == null)
-      MockBinding();
+    // if (WidgetsBinding.instance == null)
+    MockBinding();
     return WidgetsBinding.instance!;
   }
 
   MockBinaryMessenger? _binaryMessenger;
   @override
   BinaryMessenger get defaultBinaryMessenger {
-    return _binaryMessenger != null ? _binaryMessenger! : super.defaultBinaryMessenger;
+    return _binaryMessenger != null
+        ? _binaryMessenger!
+        : super.defaultBinaryMessenger;
   }
 }
 
